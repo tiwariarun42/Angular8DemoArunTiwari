4234a98b0ae6213c10faeaf0390219813179ec9c
arun tiwari <tiwariarun4246@gmail.com>
2019-07-20 18:45:25 +0530

    Newly added files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CalculatorCompComponent } from './calculator-comp/calculator-comp.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalculatorCompComponent
   ],
   imports: [
     BrowserModule,
